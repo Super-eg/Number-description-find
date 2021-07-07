42772d2001d7faf9a03fb963926638a9901e387f
Super <71580764+Super-eg@users.noreply.github.com>
2021-07-07 15:51:53 +0800

    add file

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.getElementById('type');
+const enter = document.getElementById('enter');
+let data ={text: '', num: input.value};
+
+function getText() {
+	data.num= input.value
+	fetch(`http://numbersapi.com/${data.num}`)
+	 .then(response=> response.text())
+	 .then(response=> data.text=response)
+	 document.getElementById('show').innerText = data.text;
+}
+
+function checkLength(){
+	input.value.length===0 ? alert('You should type number in the searchbox') : getText();
+}
+
+enter.addEventListener("click", function() {
+	checkLength();
+})
+
+input.addEventListener("keypress", function() {
+	if (event.keyCode===13) {
+		checkLength();
+	}
+})
