344a3130cb6e525a02b0c86b7e5ebe7f8a18c1e5
Super <71580764+Super-eg@users.noreply.github.com>
2021-07-08 12:54:47 +0800

    fix the Asynchronous

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const input = document.getElementById('type');
 const enter = document.getElementById('enter');
-let data ={text: '', num: input.value};
+let data = {text: '', num: 0};
 
 function delay(tx, tm) {
-	setTimeout(function(){
+	setTimeout(function() {
 		document.getElementById('show').innerText = tx;
 	}, tm);
 }
 
 function getText() {
 	data.num= input.value
-	fetch(`http://numbersapi.com/${data.num}`)
+	fetch(`http://numbersapi.com/${ data.num }`)
 		.then(response=> response.text())
-	  .then(response=> data.text=response);
-	let mes='wait';
-	let time = 0;
-	for (let i=0;i<3;i++){
-		time+=500;
-		mes+='..';
+		.then(response=> data.text=response);
+	let mes= 'wait';
+	let time= 0;
+	for (let i=0; i<3; i++) {
+		time+= 500;
+		mes+= '..';
 		delay(mes, time);
 	};
-	setTimeout(()=>{
+	setTimeout(function() {
 		document.getElementById('show').innerText = data.text;
 	}, 1800);
 }
 
-function checkLength(){
+function checkLength() {
 	input.value.length===0 ? alert('You should type number in the searchbox') : getText();
 }
 
