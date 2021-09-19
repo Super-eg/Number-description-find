42812c652f4b8b4b214da402e2f896e20875c6ca
Super <71580764+Super-eg@users.noreply.github.com>
2021-09-19 13:48:42 +0800

    updated

--- script.js
+++ script.js
@@ -2,27 +2,12 @@ const input = document.getElementById('type');
 const enter = document.getElementById('enter');
 let data = {text: '', num: 0};
 
-function delay(tx, tm) {
-	setTimeout(function() {
-		document.getElementById('show').innerText = tx;
-	}, tm);
-}
-
 function getText() {
 	data.num= input.value
 	fetch(`http://numbersapi.com/${ data.num }`)
 		.then(response=> response.text())
-		.then(response=> data.text=response);
-	let mes= 'wait';
-	let time= 0;
-	for (let i=0; i<3; i++) {
-		time+= 500;
-		mes+= '..';
-		delay(mes, time);
-	};
-	setTimeout(function() {
-		document.getElementById('show').innerText = data.text;
-	}, 1800);
+		.then(response=> data.text=response)
+		.then(response=> document.getElementById('show').innerText = data.text)
 }
 
 function checkLength() {
